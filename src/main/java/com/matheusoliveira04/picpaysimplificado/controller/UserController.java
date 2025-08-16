130b3c08e072b7ae42e503f61deff00a09b79e1a
MatheusOliveira04 <samatheus999@gmail.com>
2025-08-16 15:04:31 -0300

    feat: create user controller class with findById and insert methods

---
+++ src/main/java/com/matheusoliveira04/picpaysimplificado/controller/UserController.java
@@ -0,0 +1,62 @@
+package com.matheusoliveira04.picpaysimplificado.controller;
+
+import com.matheusoliveira04.picpaysimplificado.dto.request.UserRequest;
+import com.matheusoliveira04.picpaysimplificado.dto.response.UserResponse;
+import com.matheusoliveira04.picpaysimplificado.model.User;
+import com.matheusoliveira04.picpaysimplificado.model.enums.UserType;
+import com.matheusoliveira04.picpaysimplificado.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@Validated
+@RequestMapping("/v1/users")
+@RestController
+public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> findById(@PathVariable String id) {
+        User user = service.findById(UUID.fromString(id));
+        return ResponseEntity.ok(
+            new UserResponse(
+                user.getId().toString(),
+                user.getName(),
+                user.getCpfCnpj(),
+                user.getEmail(),
+                user.getType().name()
+            )
+        );
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponse> insert(@RequestBody @Valid UserRequest userRequest) {
+        User user = new User(
+            null,
+            userRequest.name(),
+            userRequest.cpfCnpj(),
+            userRequest.email(),
+            userRequest.password(),
+            UserType.fromDescription(userRequest.type())
+        );
+        User savedUser = service.insert(user);
+        return ResponseEntity.ok(
+            new UserResponse(
+                savedUser.getId().toString(),
+                savedUser.getName(),
+                savedUser.getCpfCnpj(),
+                savedUser.getEmail(),
+                savedUser.getType().name()
+            )
+        );
+    }
+
+}
