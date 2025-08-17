ca296b9a52f68b4f8bd8ad0d909261f81cca6d49
MatheusOliveira04 <samatheus999@gmail.com>
2025-08-16 23:00:33 -0300

    feat: add handler method for DataIntegrityViolationException in ControllerExceptionHandler

---
+++ src/main/java/com/matheusoliveira04/picpaysimplificado/exception/ControllerExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.matheusoliveira04.picpaysimplificado.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> getDataIntegrityViolationException(
+            DataIntegrityViolationException e,
+            HttpServletRequest request) {
+        return ResponseEntity.badRequest()
+                .body(new StandardError(
+                        LocalDateTime.now(),
+                        HttpStatus.BAD_REQUEST.value(),
+                        request.getRequestURI(),
+                        List.of(e.getMostSpecificCause().getMessage())
+                ));
+    }
+}
