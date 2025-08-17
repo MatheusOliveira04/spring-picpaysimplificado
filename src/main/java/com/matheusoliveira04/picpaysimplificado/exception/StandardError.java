f921ade9fc0172e9db4392f693123cadd10fd20f
MatheusOliveira04 <samatheus999@gmail.com>
2025-08-16 22:57:20 -0300

    feat: create exception return class

---
+++ src/main/java/com/matheusoliveira04/picpaysimplificado/exception/StandardError.java
@@ -0,0 +1,12 @@
+package com.matheusoliveira04.picpaysimplificado.exception;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record StandardError(
+        LocalDateTime timestamp,
+        Integer status,
+        String uri,
+        List<String> messages
+) {
+}
