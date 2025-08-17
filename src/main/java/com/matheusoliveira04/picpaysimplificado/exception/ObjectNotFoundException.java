b3b486a3fcc507e50931cc5d37d2d97114e816b7
MatheusOliveira04 <samatheus999@gmail.com>
2025-08-16 22:57:31 -0300

    feat: create ObjectNotFoundException

---
+++ src/main/java/com/matheusoliveira04/picpaysimplificado/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.matheusoliveira04.picpaysimplificado.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
