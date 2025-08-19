e81acd5bc66be03816ab1c34f5a88f2583a2bcf1
MatheusOliveira04 <samatheus999@gmail.com>
2025-08-18 21:23:14 -0300

    feat: add conversion from User to UserResponse

--- src/main/java/com/matheusoliveira04/picpaysimplificado/mapper/UserMapper.java
+++ src/main/java/com/matheusoliveira04/picpaysimplificado/mapper/UserMapper.java
@@ -1,9 +1,12 @@
 package com.matheusoliveira04.picpaysimplificado.mapper;
 
 import com.matheusoliveira04.picpaysimplificado.dto.request.UserRequest;
+import com.matheusoliveira04.picpaysimplificado.dto.response.UserResponse;
 import com.matheusoliveira04.picpaysimplificado.model.User;
 import com.matheusoliveira04.picpaysimplificado.model.enums.UserType;
 import org.mapstruct.*;
+
+import java.util.Optional;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
@@ -12,10 +15,23 @@ public interface UserMapper {
     @Mapping(target = "type", source = "type", qualifiedByName = "mapUserType")
     User toModel(UserRequest request);
 
+    @Mapping(target = "type", source = "type", qualifiedByName = "mapDescriptionType")
+    UserResponse toResponse(User user);
+
     @Named("mapUserType")
     default UserType mapUserType(String type) {
-        return UserType.fromDescription(type);
+        return Optional.ofNullable(type)
+                .map(UserType::fromDescription)
+                .orElseThrow(() -> new IllegalArgumentException("User type invalid: " + type));
     }
+
+    @Named("mapDescriptionType")
+    default String mapDescriptionType(UserType type) {
+        return Optional.ofNullable(type)
+                .map(UserType::getDescription)
+                .orElseThrow(() -> new IllegalArgumentException("User type cannot be null"));
+    }
+
 
 
 }
