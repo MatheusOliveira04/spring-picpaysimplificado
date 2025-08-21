9ffdd24c2bd32d202103ae35d1f398e1f372a770
MatheusOliveira04 <samatheus999@gmail.com>
2025-08-21 12:57:08 -0300

    feat: add walletBalance attribute in UserResponse

--- src/main/java/com/matheusoliveira04/picpaysimplificado/dto/response/UserResponse.java
+++ src/main/java/com/matheusoliveira04/picpaysimplificado/dto/response/UserResponse.java
@@ -1,10 +1,13 @@
 package com.matheusoliveira04.picpaysimplificado.dto.response;
 
+import java.math.BigDecimal;
+
 public record UserResponse(
         String id,
         String name,
         String cpfCnpj,
         String email,
-        String type
+        String type,
+        BigDecimal walletBalance
 ) {
 }
