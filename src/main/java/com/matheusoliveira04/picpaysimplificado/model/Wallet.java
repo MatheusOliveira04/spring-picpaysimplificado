8d45b3a39967e1c21eb666ed9db6a4d1a5cf9efe
MatheusOliveira04 <samatheus999@gmail.com>
2025-08-21 12:54:31 -0300

    feat: create Wallet class

---
+++ src/main/java/com/matheusoliveira04/picpaysimplificado/model/Wallet.java
@@ -0,0 +1,26 @@
+package com.matheusoliveira04.picpaysimplificado.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "wallets")
+@Entity(name = "wallets")
+public class Wallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @PositiveOrZero
+    @Column(nullable = false, scale = 2)
+    private BigDecimal balance;
+    @OneToOne(mappedBy = "wallet")
+    private User user;
+}
