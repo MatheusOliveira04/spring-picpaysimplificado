3cb656d5ebe3d844d39d90bcffea4fade2e4d462
MatheusOliveira04 <samatheus999@gmail.com>
2025-08-22 21:56:51 -0300

    feat: create Authorizer client to do request on mock endpoint

---
+++ src/main/java/com/matheusoliveira04/picpaysimplificado/client/ExternalAuthorizerClient.java
@@ -0,0 +1,13 @@
+package com.matheusoliveira04.picpaysimplificado.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@FeignClient(name = "externalAuthorizer", url = "https://util.devi.tools/api/v2")
+public interface ExternalAuthorizerClient {
+
+    @GetMapping("/authorize")
+    Map<String, Object> authorizeTransaction();
+}
