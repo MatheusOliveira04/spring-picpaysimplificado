5fa978e017c7f5b18f91e6f4b5c7ea1565ffdc37
MatheusOliveira04 <samatheus999@gmail.com>
2025-08-22 21:52:36 -0300

    feat: created TransferRepository class

---
+++ src/main/java/com/matheusoliveira04/picpaysimplificado/repository/TransferRepository.java
@@ -0,0 +1,11 @@
+package com.matheusoliveira04.picpaysimplificado.repository;
+
+import com.matheusoliveira04.picpaysimplificado.model.Transfer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TransferRepository extends JpaRepository<Transfer, UUID> {
+}
