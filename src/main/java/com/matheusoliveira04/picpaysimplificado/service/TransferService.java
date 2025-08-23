2f70ed2a6f02da349dd712399e03a71e70a6cae9
MatheusOliveira04 <samatheus999@gmail.com>
2025-08-23 18:34:10 -0300

    feat: add IntegrityViolationException in TransferServiceImpl and change makeTransfer to return void

--- src/main/java/com/matheusoliveira04/picpaysimplificado/service/TransferService.java
+++ src/main/java/com/matheusoliveira04/picpaysimplificado/service/TransferService.java
@@ -1,10 +1,11 @@
 package com.matheusoliveira04.picpaysimplificado.service;
 
+import com.matheusoliveira04.picpaysimplificado.dto.request.TransferRequest;
 import com.matheusoliveira04.picpaysimplificado.model.Transfer;
 import jakarta.validation.Valid;
 
 public interface TransferService {
 
-    Transfer makeTransfer(@Valid Transfer transfer);
+    void makeTransfer(@Valid TransferRequest transferRequest);
 
 }
--- src/main/java/com/matheusoliveira04/picpaysimplificado/service/impl/TransferServiceImpl.java
+++ src/main/java/com/matheusoliveira04/picpaysimplificado/service/impl/TransferServiceImpl.java
@@ -1,13 +1,20 @@
 package com.matheusoliveira04.picpaysimplificado.service.impl;
 
+import com.matheusoliveira04.picpaysimplificado.dto.request.TransferRequest;
+import com.matheusoliveira04.picpaysimplificado.exception.service.IntegrityViolationException;
 import com.matheusoliveira04.picpaysimplificado.model.Transfer;
+import com.matheusoliveira04.picpaysimplificado.model.User;
+import com.matheusoliveira04.picpaysimplificado.model.enums.UserType;
 import com.matheusoliveira04.picpaysimplificado.repository.TransferRepository;
 import com.matheusoliveira04.picpaysimplificado.service.AuthorizerService;
 import com.matheusoliveira04.picpaysimplificado.service.TransferService;
+import com.matheusoliveira04.picpaysimplificado.service.UserService;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+
+import java.util.UUID;
 
 @Validated
 @Service
@@ -15,25 +22,36 @@ public class TransferServiceImpl implements TransferService {
 
     private final TransferRepository repository;
     private final AuthorizerService authorizerService;
+    private final UserService userService;
 
-    public TransferServiceImpl(TransferRepository repository, AuthorizerService authorizerService) {
+    public TransferServiceImpl(TransferRepository repository, AuthorizerService authorizerService, UserService userService) {
         this.repository = repository;
         this.authorizerService = authorizerService;
+        this.userService = userService;
     }
 
     @Transactional
     @Override
-    public Transfer makeTransfer(@Valid Transfer transfer) {
+    public void makeTransfer(@Valid TransferRequest transferRequest) {
+
+        User payer = userService.findById(UUID.fromString(transferRequest.getPayerId()));
+        User payee = userService.findById(UUID.fromString(transferRequest.getPayeeId()));
+
+        Transfer transfer = Transfer.builder()
+                .payer(payer)
+                .payee(payee)
+                .value(transferRequest.getValue())
+                .build();
 
         //Verifica se o pagador é do tipo MERCHANT
-        /*if (UserType.MERCHANT.equals(transfer.getPayer().getType())) {
-            throw new RuntimeException("Type of user not allowed to make transfers");
-        }*/
+        if (UserType.MERCHANT.equals(transfer.getPayer().getType())) {
+            throw new IntegrityViolationException("Type of user not allowed to make transfers");
+        }
 
         // value > balance = lança exceção
-        /*if (transfer.getValue().compareTo(transfer.getPayer().getWallet().getBalance()) > 0) {
-            throw new RuntimeException("Insufficient balance to complete the transfer");
-        }*/
+       if (transfer.getValue().compareTo(transfer.getPayer().getWallet().getBalance()) > 0) {
+            throw new IntegrityViolationException("Insufficient balance to complete the transfer");
+       }
 
         //consultar um serviço autorizador externo
         // se autorizado, debitar do pagador e creditar no recebedor
@@ -48,7 +66,5 @@ public class TransferServiceImpl implements TransferService {
 
         //Enviar notificação para o recebedor - MOCK
 
-
-        return null;
     }
 }
