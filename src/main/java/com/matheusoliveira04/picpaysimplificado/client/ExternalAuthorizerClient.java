8e5b544a5f80140758546dae04f74ec3aa55c57d
MatheusOliveira04 <samatheus999@gmail.com>
2025-08-23 18:32:08 -0300

    refactor: change ExternalAuthorizerClient to return ResponseEntity<AuthorizerClientResponse>

--- src/main/java/com/matheusoliveira04/picpaysimplificado/client/ExternalAuthorizerClient.java
+++ src/main/java/com/matheusoliveira04/picpaysimplificado/client/ExternalAuthorizerClient.java
@@ -1,13 +1,13 @@
 package com.matheusoliveira04.picpaysimplificado.client;
 
+import com.matheusoliveira04.picpaysimplificado.client.dto.AuthorizerClientResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Map;
 
 @FeignClient(name = "externalAuthorizer", url = "https://util.devi.tools/api/v2")
 public interface ExternalAuthorizerClient {
 
     @GetMapping("/authorize")
-    Map<String, Object> authorizeTransaction();
+    ResponseEntity<AuthorizerClientResponse> authorizeTransaction();
 }
