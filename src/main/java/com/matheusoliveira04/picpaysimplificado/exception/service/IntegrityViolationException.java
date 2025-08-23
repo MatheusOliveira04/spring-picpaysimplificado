8058b4333fb1209e3a37e9d7a02fdf014c55bd81
MatheusOliveira04 <samatheus999@gmail.com>
2025-08-23 18:28:46 -0300

    feat: created IntegrityViolationException to uses in exceptions

---
+++ src/main/java/com/matheusoliveira04/picpaysimplificado/exception/service/IntegrityViolationException.java
@@ -0,0 +1,9 @@
+package com.matheusoliveira04.picpaysimplificado.exception.service;
+
+public class IntegrityViolationException extends RuntimeException {
+
+    public IntegrityViolationException(String message) {
+        super(message);
+    }
+
+}
