f59aadffb364fe03cfbdef0c541a874126567bcf
MatheusOliveira04 <samatheus999@gmail.com>
2025-08-28 18:25:41 -0300

    feat: created debit and credit method in Wallet class to apply transfer in TransferServiceImpl

--- src/main/java/com/matheusoliveira04/picpaysimplificado/model/Wallet.java
+++ src/main/java/com/matheusoliveira04/picpaysimplificado/model/Wallet.java
@@ -1,5 +1,6 @@
 package com.matheusoliveira04.picpaysimplificado.model;
 
+import com.matheusoliveira04.picpaysimplificado.exception.service.IntegrityViolationException;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
@@ -18,9 +19,29 @@ public class Wallet {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+    @Setter
     @PositiveOrZero
     @Column(nullable = false, scale = 2)
     private BigDecimal balance;
     @OneToOne(mappedBy = "wallet", fetch = FetchType.LAZY)
     private User user;
+
+    public void credit(BigDecimal amount) {
+        if (amount.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IntegrityViolationException("Amount to credit must be positive");
+        } else {
+            this.balance = this.balance.add(amount);
+        }
+    }
+
+    public void debit(BigDecimal amount) {
+        if (amount.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IntegrityViolationException("Amount to debit must be positive");
+        } else if (this.balance.compareTo(amount) < 0) {
+            throw new IntegrityViolationException("Insufficient balance to debit the specified amount");
+        } else {
+            this.balance = this.balance.subtract(amount);
+        }
+    }
+
 }
